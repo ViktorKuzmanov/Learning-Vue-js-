b1a3b6430417a3fc233aaacd6881224f4bbbb00f
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-12-09 10:06:24 +0100

    event handling - increase counter

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const app = Vue.createApp({
     data() {
         return {
           name: 'CodeNerd',
+          counter: 0
         }
+      },
+    
+    methods: {
+      increaseCounter() {
+        this.counter += 1
       }
+    }
   })
   
   app.mount('#app')
